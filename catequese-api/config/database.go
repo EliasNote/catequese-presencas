bee56fb82322ed98342ff10fabbafbe4b7e9df9d
elias-made <elias.sand@madeinweb.com.br>
2026-02-28 16:05:05 -0300

    feat

--- catequese-api/config/database.go
+++ catequese-api/config/database.go
@@ -2,14 +2,25 @@ package config
 
 import (
 	"catequese-api/models"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase() *gorm.DB {
-    dsn := "root:root@tcp(localhost:3306)/catedral?charset=utf8mb4&parseTime=True&loc=Local"
+	_ = godotenv.Load()
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
@@ -24,4 +35,12 @@ func ConnectDatabase() *gorm.DB {
 	)
 
     return db
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }

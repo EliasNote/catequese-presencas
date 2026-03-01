1e0955b6504399772dd2dbf981aa41e1be802d20
elias-made <elias.sand@madeinweb.com.br>
2026-03-01 14:09:03 -0300

    feat

--- catequese-api/config/database.go
+++ catequese-api/config/database.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -20,21 +20,21 @@ func ConnectDatabase() *gorm.DB {
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=America/Sao_Paulo", dbHost, dbUser, dbPassword, dbName, dbPort)
 
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Falha ao conectar no banco:", err)
-    }
-    
-    db.AutoMigrate(
-				&models.Comunidade{},
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Falha ao conectar no banco:", err)
+	}
+
+	db.AutoMigrate(
+		&models.Comunidade{},
 		&models.Catequizando{},
 		&models.Encontro{},
 		&models.Presenca{},
 	)
 
-    return db
+	return db
 }
 
 func getEnv(key, defaultValue string) string {
--- catequese-api/main.go
+++ catequese-api/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"catequese-api/config"
 	"catequese-api/routes"
+	"os"
 )
 
 func main() {
 	db := config.ConnectDatabase()
 	r := routes.SetupRoutes(db)
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    r.Run(":" + port) 
 }
